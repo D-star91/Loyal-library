89ef1d8422784e3274c26fee2ccce4dbd2270480
D Star <daniel123lay@gmail.com>
2021-08-20 16:01:25 +0630

    verse bottom

--- resources/views/Bible/verse.blade.php
+++ resources/views/Bible/verse.blade.php
@@ -78,23 +78,23 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="vs-card-contant d-grid gap-2">
-                        <button class="btn-vs-title btn-vs-link" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2" >
-                            <h3 class="vs-h3">ယုံကြည်ခြင်း</h3>
+                        <button class="btn-vs-title btn-vs-link2" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2" >
+                            <h3 class="vs-h3">ယုံကြည်ခြင်းနှင့်ဆိုင်သောကျမ်းချက်များ</h3>
                         </button>
                         <div class="collapse mt-3" id="collapseExample2">
                             <div class="vs-bottom">
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">January</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">February</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">March</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">April</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">May</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">June</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">July</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">August</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">September</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">October</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">November</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">December</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">January</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">February</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">March</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">April</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">May</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">June</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">July</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">August</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">September</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">October</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">November</h3></a>
+                                <a href="" class="btn-vs btn-vs-link2"><h3 class="vs-h3 bd">December</h3></a>
                             </div>
                         </div>
                     </div>
@@ -103,23 +103,23 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="vs-card-contant d-grid gap-2">
-                        <button class="btn-vs-title btn-vs-link" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample2" >
-                            <h3 class="vs-h3">တစ်လတာကျမ်းချက်များ</h3>
+                        <button class="btn-vs-title btn-vs-link3" type="button" data-mdb-toggle="collapse" data-mdb-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample2" >
+                            <h3 class="vs-h3">ချစ်ခြင်းမေတ္တာနှင့်ဆိုင်သောကျမ်းချက်များ</h3>
                         </button>
                         <div class="collapse mt-3 " id="collapseExample3">
                             <div class="vs-bottom">
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">January</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">February</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">March</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">April</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">May</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">June</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">July</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">August</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">September</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">October</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">November</h3></a>
-                                <a href="" class="btn-vs btn-vs-link"><h3 class="vs-h3 bd">December</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">January</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">February</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">March</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">April</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">May</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">June</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">July</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">August</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">September</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">October</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">November</h3></a>
+                                <a href="" class="btn-vs btn-vs-link3"><h3 class="vs-h3 bd">December</h3></a>
                             </div>
                         </div>
                     </div>
